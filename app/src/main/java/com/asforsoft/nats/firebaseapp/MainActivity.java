cbb0a1f6c47d8afa196e70c716b40b58ca4cd0d2
nats <nocturno1470@gmail.com>
2018-10-31 17:08:50 -0500

    initial commit

---
+++ app/src/main/java/com/asforsoft/nats/firebaseapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.asforsoft.nats.firebaseapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
